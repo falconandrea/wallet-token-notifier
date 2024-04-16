a6d0f462776256e91645b21ba7ec9940f48c6575
falconandrea <falcon.andrea88@gmail.com>
2024-04-16 23:41:38 +0200

    feat: alert for ETH

---
+++ index.js
@@ -0,0 +1,86 @@
+// Load DotEnv
+require("dotenv").config();
+const fs = require("fs");
+const Discord = require("discord.js");
+const EmbedBuilder = require("discord.js").EmbedBuilder;
+const client = new Discord.Client({
+  intents: [
+    "Guilds",
+    "GuildMessages",
+    "MessageContent",
+    "GuildMessages",
+    "GuildMembers",
+  ],
+});
+const discordToken = process.env.DISCORD_CLIENT_TOKEN;
+client.login(discordToken);
+
+const wallets = process.env.ETH_WALLETS.split(",") || [];
+
+const lastBlocks = require("./last_blocks.json");
+// Convert all JSON uppercase
+Object.entries(lastBlocks).forEach(([network, wallets]) => {
+  lastBlocks[network] = Object.fromEntries(
+    Object.entries(wallets).map(([wallet, block]) => [
+      wallet.toUpperCase(),
+      block,
+    ])
+  );
+});
+
+const scanLinks = {
+  ETH: `https://api.etherscan.io/api?module=account&action=tokentx&page=1&offset=100&&endblock=27025780&sort=desc&apikey=${process.env.ETHERSCAN_APIKEY}`,
+};
+
+const getResult = async (url, chain, wallet, startblock) => {
+  const urlToCall = `${url}&address=${wallet}&startblock=${startblock}`;
+  const result = await fetch(urlToCall).then((res) => res.json());
+  const txs = result.result;
+  const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID);
+
+  wallet = wallet.toUpperCase();
+
+  if (txs.length > 0) {
+    // Update the block numbers inside array
+    lastBlocks[chain][wallet] = parseInt(txs[0].blockNumber) + 1;
+
+    // Check possible airdrops
+    for (const tx of txs) {
+      if (tx.to.toUpperCase() === wallet) {
+        // Send message to Discord
+        const message = new EmbedBuilder()
+          .setTitle("New token received")
+          .addFields(
+            { name: "Chain", value: chain },
+            { name: "Wallet", value: wallet },
+            { name: "Token Name", value: tx.tokenName },
+            { name: "Token Symbol", value: tx.tokenSymbol },
+            { name: "Contract Address", value: tx.contractAddress }
+          );
+        channel.send({ embeds: [message] });
+      }
+    }
+  }
+};
+
+const main = async () => {
+  for (const [network, url] of Object.entries(scanLinks)) {
+    for (const wallet of wallets) {
+      console.log(network, wallet);
+      const startblock = lastBlocks[network][wallet.toUpperCase()] || 0;
+
+      await getResult(url, network, wallet, startblock);
+
+      // Wait for 5 seconds
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+    }
+  }
+
+  // Update the json with the last block numbers
+  fs.writeFileSync("./last_blocks.json", JSON.stringify(lastBlocks, null, 2));
+};
+
+// Call main function and terminate the script
+main().then(() => {
+  process.exit(0);
+});
